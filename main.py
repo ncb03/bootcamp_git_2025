d43deb868ee1c0c5971ad5f36c49f1234c8b69be
ncb03 <caneesha.neufahrt@otto.de>
2025-09-15 15:47:49 +0200

    Delete function main

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ num1 = float(input("Number1?"))
 num2 = float(input("Number 2?"))
 op = input("Operator? Choose between +, -, * and /")
 
-def main():
-    x = 2.0
-    y = 4.0
-
-    print(f"{x} + {y} = {add(x, y)}")
 
 
 def add(x, y):
