9cd0e0830c6c0712fb42a9930abd97b900811e71
ncb03 <caneesha.neufahrt@otto.de>
2025-09-15 16:00:58 +0200

    Add logic for add

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ print("Hello User")
 
 num1 = float(input("Number1?"))
 num2 = float(input("Number 2?"))
-op = input("Operator? Choose between +, -, * and /")
-
+op = input("Operator? Choose between +, -, * and /:")
 
 def add(x, y):
     return x + y
@@ -20,7 +19,13 @@ def mul(x, y):
 def div(x, y):
     return x / y
 
+match op:
+    case "+":
+        print(add(num1, num2))
 
-if __name__ == '__main__':
-    main()
+
+
+
+# if __name__ == '__main__':
+#     main()
 
